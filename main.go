538faf984587779d5df585479545268af8345fb2
Tamino Hartmann <tamino.hartmann@gmail.com>
2015-10-02 15:06:53 +0200

    added terrible and incorrect Build method

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"sort"
+	"time"
 
 	"github.com/tinzenite/shared"
 )
@@ -46,6 +47,33 @@ func LoadFrom(path string) (*Model, error) {
 		return nil, err
 	}
 	return m, nil
+}
+
+/*
+Build creates a model from an ObjectInfo tree. NOTE: not to be used for creating
+models that work on a file system without setting valid paths!
+*/
+func Build(object *shared.ObjectInfo) *Model {
+	m := &Model{
+		RootPath:     "",
+		TrackedPaths: make(map[string]bool),
+		StaticInfos:  make(map[string]staticinfo),
+		SelfID:       "",
+		StorePath:    ""}
+	// we only call time.Now() once because we don't need perfect accuracy
+	modtime := time.Now()
+	// apply each object to the model
+	object.ForEach(func(this shared.ObjectInfo) {
+		m.TrackedPaths[this.Path] = true
+		m.StaticInfos[this.Path] = staticinfo{
+			Identification: this.Identification,
+			Directory:      this.Directory,
+			Content:        this.Content,
+			Modtime:        modtime,
+			Version:        this.Version}
+	})
+	// done, so return model
+	return m
 }
 
 /*
